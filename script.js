9987d6571706dc5e887967061608c10dad155580
Anton <112634784+Anton718@users.noreply.github.com>
2023-03-27 18:53:56 +0000

    add sort

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const btn_back = document.querySelector('.back')
 const div_cards = document.querySelector('.cards')
 const div_buttons = document.querySelector('.buttons')
 const select = document.getElementById('worlds')
+const clear = document.querySelector('.clear')
 
 async function prepare() {
     let response = await fetch('https://rickandmortyapi.com/api/character');
@@ -13,6 +14,7 @@ async function prepare() {
         data = await response.json();
         data.results.forEach(elem => { div_cards.append(create_card(elem)) })
         create_options(data)
+        sort_cards()
     } else {alert(`failure to fetch data from https://rickandmortyapi.com/api/character`)}
    
 }
@@ -45,19 +47,25 @@ function filterData() {
             for (let y of data.results) {  
                 if (y.location.name === x.value) {
                 div_cards.append(create_card(y))
-            };
+            } 
         }
     } 
+} 
 }
-}
+
+clear.addEventListener('click', function clearInput() {
+search_input.value = '';
+data.results.forEach(elem => { div_cards.append(create_card(elem)) })
+
+})
+
 
 
 const search_input = document.createElement('input')
-const _input = document.createElement('input')
 
 div_buttons.append(search_input)
-search_input.style.width = '300px'
-search_input.style.height = '40px'
+search_input.style.width = '200px'
+search_input.style.height = '30px'
 search_input.placeholder = 'find character'
 
 
@@ -66,22 +74,27 @@ function sort_cards() {
     let array = data.results.filter(elem => elem.name.toLowerCase().includes(search_input.value.trim()))
     div_cards.innerHTML = "";
     array.forEach(elem => div_cards.append(create_card(elem)))
+
     return div_cards
 }
 
 
-function create_card(object) {
-const div_card = document.createElement('div')
-const cardImage = document.createElement('img')
-const name1 = document.createElement('h2')
-const species = document.createElement('span')
-    cardImage.src = object.image
-    name1.textContent = object.name
-    species.textContent = object.species
 
-    div_card.append(cardImage)
-    div_card.append(name1)
-    div_card.append(species)
+
+
+function create_card(object) {
+const div_card = document.createElement('div');
+div_card.className = 'div_one';
+const cardImage = document.createElement('img');
+const name1 = document.createElement('h2');
+const species = document.createElement('span');
+    cardImage.src = object.image;
+    name1.textContent = object.name;
+    species.textContent = object.species;
+
+    div_card.append(cardImage);
+    div_card.append(name1);
+    div_card.append(species);
     
 return div_card
 }
@@ -96,6 +109,7 @@ async function prepare() {
         data = await response.json();
         data.results.forEach(elem => { div_cards.append(create_card(elem)) })
     create_options(data)
+    sort_cards()
 
 }
 prepare() 
@@ -114,6 +128,8 @@ async function prepare() {
         data = await response.json();
         data.results.forEach(elem => { div_cards.append(create_card(elem)) })
     create_options(data)
+    sort_cards()
+
 
 }
 prepare() 
